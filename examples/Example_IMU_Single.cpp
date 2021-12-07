0b90a2c68b437ac29ddc904ad6cef5220a0d0345
Tim Pfeifer <33322971+tipf@users.noreply.github.com>
2021-12-07 11:11:24 +0100

    Add example files

---
+++ examples/Example_IMU_Single.cpp
@@ -0,0 +1,143 @@
+/***************************************************************************
+ * libRSF - A Robust Sensor Fusion Library
+ *
+ * Copyright (C) 2020 Chair of Automation Technology / TU Chemnitz
+ * For more information see https://www.tu-chemnitz.de/etit/proaut/libRSF
+ *
+ * libRSF is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libRSF is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libRSF.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Tim Pfeifer (tim.pfeifer@etit.tu-chemnitz.de)
+ ***************************************************************************/
+
+/**
+* @file Example_IMU_Single.cpp
+* @author Tim Pfeifer
+* @date 27.08.2020
+* @brief An example application to test the IMU factors with a single measurement.
+* @copyright GNU Public License.
+*
+*/
+
+#include "libRSF.h"
+
+#define POSITION_STATE "Position"
+#define ORIENTATION_STATE "Orientation"
+#define IMU_STATE "IMUBias"
+
+int main(int ArgC, char** ArgV)
+{
+  (void)ArgC;
+  google::InitGoogleLogging(ArgV[0]);
+
+  /** create measurement mean */
+  libRSF::Vector6 MeanIMU;
+  MeanIMU << 0.1, 0, 0, 0, 0, 0.1;
+
+  double dt = 0.5;
+
+  /** add gravity */
+  MeanIMU(2) += 9.81;
+
+  /** create sensor object */
+  libRSF::Data IMU(libRSF::DataType::IMU, dt);
+  IMU.setMean(MeanIMU);
+
+  /** create graph */
+  libRSF::FactorGraph Graph;
+
+  Graph.addStateWithCheck(POSITION_STATE, libRSF::DataType::Point3, 0.0);
+  Graph.addStateWithCheck(ORIENTATION_STATE, libRSF::DataType::Quaternion, 0.0);
+  Graph.addStateWithCheck(IMU_STATE, libRSF::DataType::IMUBias, 0.0);
+
+  Graph.addStateWithCheck(POSITION_STATE, libRSF::DataType::Point3, dt);
+  Graph.addStateWithCheck(ORIENTATION_STATE, libRSF::DataType::Quaternion, dt);
+  Graph.addStateWithCheck(IMU_STATE, libRSF::DataType::IMUBias, dt);
+
+  /** freeze first values */
+  Graph.setConstant(POSITION_STATE, 0.0);
+  Graph.setConstant(ORIENTATION_STATE, 0.0);
+  Graph.setConstant(IMU_STATE, 0.0);
+
+  /** pre-integrate measurements */
+  libRSF::IMUPreintegrator PreInt(libRSF::Vector3::Zero(),/**< bias accelerometer  */
+                                  libRSF::Vector3::Zero(),/**< bias gyrometer */
+                                  1.0,  /**< noise density acc. [m/s^2/sqrt(Hz)]*/
+                                  1.0,  /**< noise density gyro [rad/s/sqrt(Hz)]*/
+                                  1.0,  /**< random walk acc. [m/s^2]*/
+                                  1.0,  /**< random walk gyro. [rad/s]*/
+                                  0.0); /**< current timestamp */
+  IMU.setTimestamp(dt);
+  PreInt.addMeasurement(IMU); /** there is only one IMU measurement in this example */
+
+  /** add factor */
+  libRSF::StateList IMUStates;
+  IMUStates.add(POSITION_STATE, 0.0);
+  IMUStates.add(ORIENTATION_STATE, 0.0);
+  IMUStates.add(IMU_STATE, 0.0);
+  IMUStates.add(POSITION_STATE, dt);
+  IMUStates.add(ORIENTATION_STATE, dt);
+  IMUStates.add(IMU_STATE, dt);
+  Graph.addIMUPreintegrationFactor(IMUStates, PreInt.getPreintegratedState());
+
+  /** configure the solver */
+  ceres::Solver::Options SolverOptions;
+  SolverOptions.minimizer_progress_to_stdout = true;
+  SolverOptions.trust_region_strategy_type = ceres::TrustRegionStrategyType::DOGLEG;
+  SolverOptions.dogleg_type = ceres::DoglegType::SUBSPACE_DOGLEG;
+
+  SolverOptions.max_num_iterations = 1000;
+  SolverOptions.max_solver_time_in_seconds = 10;
+  SolverOptions.num_threads = 1;
+
+  /** optimize */
+  Graph.solve(SolverOptions);
+  Graph.printReport();
+  Graph.computeCovariance(POSITION_STATE, dt);
+  Graph.computeCovariance(ORIENTATION_STATE, dt);
+  Graph.computeCovariance(IMU_STATE, dt);
+
+  /** print result */
+  std::cout << "##### estimated states #####" << std::endl;
+  std::cout << "Translation:" << std::endl
+            << Graph.getStateData().getElement(POSITION_STATE, dt).getMean().transpose()
+            << std::endl << "Covariance:"<< std::endl
+            << libRSF::MatrixRef<double,3,3>(Graph.getStateData().getElement(POSITION_STATE, dt).getDataPointer(libRSF::DataElement::Covariance))
+            << std::endl << std::endl;
+
+  std::cout << "Rotation:" << std::endl
+            << Graph.getStateData().getElement(ORIENTATION_STATE, dt).getMean().transpose()
+            << std::endl << "Covariance:"<< std::endl
+            << libRSF::MatrixRef<double,4,4>(Graph.getStateData().getElement(ORIENTATION_STATE, dt).getDataPointer(libRSF::DataElement::Covariance))
+            << std::endl << std::endl;
+
+  std::cout << "Speedbias:" << std::endl
+            << Graph.getStateData().getElement(IMU_STATE, dt).getMean().transpose()
+            << std::endl << "Covariance:"<< std::endl
+            << libRSF::MatrixRef<double,9,9>(Graph.getStateData().getElement(IMU_STATE, dt).getDataPointer(libRSF::DataElement::Covariance));
+
+  std::cout << std::endl << "##### pre-integrated  values #####" << std::endl;
+  std::cout << "Translation:" << std::endl << PreInt.getPreintegratedState().Translation.transpose()  << std::endl << std::endl;
+  std::cout << "Rotation:" << std::endl << PreInt.getPreintegratedState().Rotation.coeffs().transpose() << std::endl << std::endl;
+  std::cout << "Velocity:" << std::endl << PreInt.getPreintegratedState().Velocity.transpose()  << std::endl;
+  std::cout << std::endl << "Covariance:" << std::endl << PreInt.getPreintegratedState().PreIntCov << std::endl << std::endl;
+
+  std::cout << std::endl << "Jacobians" << std::endl;
+  std::cout << "J_TA: " << std::endl << PreInt.getPreintegratedState().JacTransBiasAcc << std::endl << std::endl;
+  std::cout << "J_TG: " << std::endl << PreInt.getPreintegratedState().JacVelBiasTR << std::endl << std::endl;
+  std::cout << "J_VA: " << std::endl << PreInt.getPreintegratedState().JacVelBiasAcc << std::endl << std::endl;
+  std::cout << "J_VG: " << std::endl << PreInt.getPreintegratedState().JacVelBiasTR << std::endl << std::endl;
+  std::cout << "J_RG: " << std::endl << PreInt.getPreintegratedState().JacRotBiasTRLocal << std::endl << std::endl;
+
+  return 0;
+}
---
+++ examples/Example_Odometry.cpp
@@ -0,0 +1,176 @@
+/***************************************************************************
+ * libRSF - A Robust Sensor Fusion Library
+ *
+ * Copyright (C) 2020 Chair of Automation Technology / TU Chemnitz
+ * For more information see https://www.tu-chemnitz.de/etit/proaut/libRSF
+ *
+ * libRSF is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libRSF is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libRSF.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Tim Pfeifer (tim.pfeifer@etit.tu-chemnitz.de)
+ ***************************************************************************/
+
+  /**
+* @file Example_Odometry.cpp
+* @author Tim Pfeifer
+* @date 19.05.2020
+* @brief An example application to compare different odometry implementations.
+* @copyright GNU Public License.
+*
+*/
+
+#include "libRSF.h"
+
+#define COV_VEL 0.1
+#define COV_TR 0.1
+
+#define STEPS 5
+
+int main(int ArgC, char** ArgV)
+{
+  (void)ArgC;
+  google::InitGoogleLogging(ArgV[0]);
+
+  /** create odometry data */
+  libRSF::Vector3 Velocity;
+  Velocity.x() = 1.0;
+  Velocity.y() = 0.1;
+  Velocity.z() = 0.1;
+
+  libRSF::Vector3 TurnRate;
+  TurnRate.x() = 0.001;
+  TurnRate.y() = 0.001;
+  TurnRate.z() = M_PI*2.0*0.1;
+
+  std::vector<libRSF::Data> OdomMeasurements;
+  for (int t = 0; t < STEPS; t++)
+  {
+    const double Time = (t + 1)*0.1;
+
+    /** fixed movement */
+    libRSF::Data Odom(libRSF::DataType::Odom3, Time);
+    Odom.setMean((libRSF::Vector6() << Velocity, TurnRate).finished());
+    Odom.setCovarianceDiagonal((libRSF::Vector6() << COV_VEL, COV_VEL, COV_VEL, COV_TR, COV_TR, COV_TR).finished());
+    OdomMeasurements.emplace_back(Odom);
+  }
+
+  /** create factor graph */
+  libRSF::FactorGraph Graph;
+  Graph.addState("Position_Odom", libRSF::DataType::Point3, 0.0);
+  Graph.addState("Position_Odom_Int", libRSF::DataType::Point3, 0.0);
+
+  Graph.addState("Quaternion_Odom", libRSF::DataType::Quaternion, 0.0);
+  Graph.addState("Quaternion_Odom_Int", libRSF::DataType::Quaternion, 0.0);
+
+  /** freeze first state */
+  Graph.setConstant("Position_Odom", 0.0);
+  Graph.setConstant("Position_Odom_Int", 0.0);
+
+  Graph.setConstant("Quaternion_Odom", 0.0);
+  Graph.setConstant("Quaternion_Odom_Int", 0.0);
+
+  /** normal (non-integrating) factor */
+  double LastTimestamp = 0.0;
+  for (const libRSF::Data &Odom : OdomMeasurements)
+  {
+    const double TimeStamp = Odom.getTimestamp();
+
+    /** add state */
+    Graph.addState("Position_Odom", libRSF::DataType::Point3, TimeStamp);
+    Graph.addState("Quaternion_Odom", libRSF::DataType::Quaternion, TimeStamp);
+
+    /** error model */
+    libRSF::GaussianDiagonal<6> Noise;
+    Noise.setCovarianceDiagonal(Odom.getCovarianceDiagonal());
+
+    /** add factor */
+    Graph.addFactor<libRSF::FactorType::Odom6>(libRSF::StateID("Position_Odom", LastTimestamp),
+                                               libRSF::StateID("Quaternion_Odom", LastTimestamp),
+                                               libRSF::StateID("Position_Odom", TimeStamp),
+                                               libRSF::StateID("Quaternion_Odom", TimeStamp),
+                                               Odom, Noise);
+    LastTimestamp = TimeStamp;
+  }
+
+  /** integration and pose factor */
+  libRSF::OdometryIntegrator OdomInt;
+  LastTimestamp = 0.0;
+  for (const libRSF::Data &Odom : OdomMeasurements)
+  {
+    const double TimeStamp = Odom.getTimestamp();
+
+    /** integrate */
+    OdomInt.addMeasurement(Odom, TimeStamp-LastTimestamp);
+
+    /** get integrated pose */
+    libRSF::Data PoseInt(libRSF::DataType::Pose3, TimeStamp);
+    PoseInt.setMean(OdomInt.getJointPose());
+
+    /** get integrated noise model */
+    libRSF::GaussianFull<6> NoiseInt;
+    NoiseInt.setCovarianceMatrix(OdomInt.getJointCovOnManifold());
+
+    /** add integrated factor (Normally, we would just integrate all and add one factor. This is for evaluation of the intermediate steps.) */
+    Graph.addState("Position_Odom_Int", libRSF::DataType::Point3, TimeStamp);
+    Graph.addState("Quaternion_Odom_Int", libRSF::DataType::Quaternion, TimeStamp);
+    Graph.addFactor<libRSF::FactorType::BetweenPose3>(libRSF::StateID("Position_Odom_Int", 0.0),
+                                                      libRSF::StateID("Quaternion_Odom_Int", 0.0),
+                                                      libRSF::StateID("Position_Odom_Int", TimeStamp),
+                                                      libRSF::StateID("Quaternion_Odom_Int", TimeStamp),
+                                                      PoseInt, NoiseInt);
+
+    LastTimestamp = TimeStamp;
+  }
+
+  /** optimize both graphs */
+  Graph.solve();
+  Graph.computeCovariance("Position_Odom");
+  Graph.computeCovariance("Position_Odom_Int");
+
+  Graph.computeCovariance("Quaternion_Odom");
+  Graph.computeCovariance("Quaternion_Odom_Int");
+
+  /** output result */
+  for (const libRSF::Data &Odom : OdomMeasurements)
+  {
+    const double TimeStamp = Odom.getTimestamp();
+
+    std::cout <<"########################### Timestamp: "<< TimeStamp << " ###########################" <<std::endl;
+
+    std::cout << "Factor  Odom - Translation: " << Graph.getStateData().getElement("Position_Odom", TimeStamp).getMean().transpose()
+                                        << " ### "
+                                        << Graph.getStateData().getElement("Position_Odom", TimeStamp).getCovarianceDiagonal().transpose()
+                                        << std::endl;
+
+    std::cout << "Integr. Odom - Translation: " << Graph.getStateData().getElement("Position_Odom_Int", TimeStamp).getMean().transpose()
+                                        << " ### "
+                                        << Graph.getStateData().getElement("Position_Odom_Int", TimeStamp).getCovarianceDiagonal().transpose()
+                                        << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Factor  Odom - Rotation:   " << Graph.getStateData().getElement("Quaternion_Odom", TimeStamp).getMean().transpose()
+                                        << " ### "
+                                        << Graph.getStateData().getElement("Quaternion_Odom", TimeStamp).getCovarianceDiagonal().transpose()
+                                        << std::endl;
+
+    std::cout << "Integr. Odom - Rotation:   " << Graph.getStateData().getElement("Quaternion_Odom_Int", TimeStamp).getMean().transpose()
+                                        << " ### "
+                                        << Graph.getStateData().getElement("Quaternion_Odom_Int", TimeStamp).getCovarianceDiagonal().transpose()
+                                        << std::endl;
+
+    std::cout << std::endl;
+  }
+
+  return 0;
+}
