1ef751a3fd29d8ba31ec0843df1b1533d8bd5be0
Tim Pfeifer <33322971+tipf@users.noreply.github.com>
2026-02-26 22:32:19 +0100

    Modernize dependency handling

--- cmake/FindGeographicLib.cmake
+++ cmake/FindGeographicLib.cmake
@@ -10,32 +10,61 @@ find_library (GeographicLib_LIBRARIES
   NAMES GeographicLib Geographic
   PATHS "${CMAKE_INSTALL_PREFIX}/../GeographicLib/lib")
 
+find_path (GeographicLib_INCLUDE_DIRS
+  NAMES GeographicLib/Config.h
+  PATHS
+    "${CMAKE_INSTALL_PREFIX}/../GeographicLib/include"
+    "${CMAKE_INSTALL_PREFIX}/include")
+
+unset (GeographicLib_LIBRARY_DIRS)
 if (GeographicLib_LIBRARIES)
   get_filename_component (GeographicLib_LIBRARY_DIRS
     "${GeographicLib_LIBRARIES}" PATH)
-  get_filename_component (_ROOT_DIR "${GeographicLib_LIBRARY_DIRS}" PATH)
-  set (GeographicLib_INCLUDE_DIRS "${_ROOT_DIR}/include")
-  set (GeographicLib_BINARY_DIRS "${_ROOT_DIR}/bin")
-  if (NOT EXISTS "${GeographicLib_INCLUDE_DIRS}/GeographicLib/Config.h")
-    # On Debian systems the library is in e.g.,
-    #   /usr/lib/x86_64-linux-gnu/libGeographic.so
-    # so try stripping another element off _ROOT_DIR
-    get_filename_component (_ROOT_DIR "${_ROOT_DIR}" PATH)
-    set (GeographicLib_INCLUDE_DIRS "${_ROOT_DIR}/include")
-    set (GeographicLib_BINARY_DIRS "${_ROOT_DIR}/bin")
-    if (NOT EXISTS "${GeographicLib_INCLUDE_DIRS}/GeographicLib/Config.h")
-      unset (GeographicLib_INCLUDE_DIRS)
-      unset (GeographicLib_LIBRARIES)
-      unset (GeographicLib_LIBRARY_DIRS)
-      unset (GeographicLib_BINARY_DIRS)
-    endif ()
+endif ()
+
+unset (GeographicLib_BINARY_DIRS)
+if (GeographicLib_INCLUDE_DIRS)
+  get_filename_component (_GeographicLib_PREFIX "${GeographicLib_INCLUDE_DIRS}" PATH)
+  if (EXISTS "${_GeographicLib_PREFIX}/bin")
+    set (GeographicLib_BINARY_DIRS "${_GeographicLib_PREFIX}/bin")
   endif ()
-  unset (_ROOT_DIR)
+  unset (_GeographicLib_PREFIX)
+endif ()
+
+if (NOT GeographicLib_BINARY_DIRS)
+  find_program (_GeographicLib_TOOL
+    NAMES GeodSolve GeoConvert)
+  if (_GeographicLib_TOOL)
+    get_filename_component (GeographicLib_BINARY_DIRS "${_GeographicLib_TOOL}" PATH)
+  endif ()
+  unset (_GeographicLib_TOOL CACHE)
+  unset (_GeographicLib_TOOL)
 endif ()
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (GeographicLib DEFAULT_MSG
   GeographicLib_LIBRARY_DIRS GeographicLib_LIBRARIES
   GeographicLib_INCLUDE_DIRS)
+
+if(GeographicLib_FOUND)
+  set(GEOGRAPHICLIB_FOUND TRUE)
+endif()
+
+if(GeographicLib_FOUND AND NOT TARGET GeographicLib::GeographicLib)
+  # Determine the library type from the file extension so that target
+  # introspection (get_target_property TYPE) returns the correct value.
+  if(GeographicLib_LIBRARIES MATCHES "\\.a$")
+    set(_geographiclib_lib_type STATIC)
+  else()
+    set(_geographiclib_lib_type SHARED)
+  endif()
+  add_library(GeographicLib::GeographicLib ${_geographiclib_lib_type} IMPORTED)
+  set_target_properties(GeographicLib::GeographicLib PROPERTIES
+    IMPORTED_LOCATION "${GeographicLib_LIBRARIES}"
+    INTERFACE_INCLUDE_DIRECTORIES "${GeographicLib_INCLUDE_DIRS}"
+  )
+  unset(_geographiclib_lib_type)
+endif()
+
 mark_as_advanced (GeographicLib_LIBRARY_DIRS GeographicLib_LIBRARIES
   GeographicLib_INCLUDE_DIRS)
